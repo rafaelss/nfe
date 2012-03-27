11b872fd96b22b7d358a61a6383424e94c44b716
Rafael Souza <me@rafaelss.com>
2012-03-26 23:31:14 -0300

    we can have multiple "det" tags

--- lib/nfe/template/inf_nfe.rb
+++ lib/nfe/template/inf_nfe.rb
@@ -1,8 +1,8 @@
 module Nfe
   module Template
     class InfNfe < Base
-      attr_accessor :id
-      attr_writer :ide, :emit, :dest, :det, :total, :transp, :cobr, :inf_adic
+      attr_accessor :id, :dets
+      attr_writer :ide, :emit, :dest, :total, :transp, :cobr, :inf_adic
 
       def ide
         @ide.render
@@ -14,10 +14,6 @@ module Nfe
 
       def dest
         @dest.render
-      end
-
-      def det
-        @det.render
       end
 
       def total
