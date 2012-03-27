3714a48598082ed0c7e7b2284a192e81fb08293a
Rafael Souza <me@rafaelss.com>
2012-03-27 02:17:03 -0300

    format the values with two decimals

--- lib/nfe/template/fat.rb
+++ lib/nfe/template/fat.rb
@@ -1,7 +1,16 @@
 module Nfe
   module Template
     class Fat < Base
-      attr_accessor :n_fat, :v_orig, :v_liq
+      attr_accessor :n_fat
+      attr_writer :v_orig, :v_liq
+
+      def v_orig
+        "%0.2f" % @v_orig
+      end
+
+      def v_liq
+        "%0.2f" % @v_liq
+      end
     end
   end
 end
